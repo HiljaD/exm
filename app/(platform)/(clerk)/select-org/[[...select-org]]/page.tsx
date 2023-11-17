3e346f204751a50ce042e99c5a4b1a332d8f3c0c
Jari <jarirapani.turunen@gmail.com>
2023-11-17 17:41:47 +0200

    korjaus

--- app/(platform)/(clerk)/select-org/[[...select-org]]/page.tsx
+++ app/(platform)/(clerk)/select-org/[[...select-org]]/page.tsx
@@ -6,6 +6,7 @@ export default function CreateOrganizationPage() {
       hidePersonal
       afterSelectOrganizationUrl="/organization/:id"
       afterCreateOrganizationUrl="/organization/:id"
+      ///afterLeaveOrganizationUrl="/select-org"
     />
   );
 };
