4adb6361b46a0068b887af5446d2e60b2aa84396
Jari <jarirapani.turunen@gmail.com>
2023-11-22 17:49:15 +0200

    List components

--- app/(platform)/(dashboard)/board/[boardId]/page.tsx
+++ app/(platform)/(dashboard)/board/[boardId]/page.tsx
@@ -1,10 +1,50 @@
+import { auth } from "@clerk/nextjs";
+import { redirect } from "next/navigation";
 
-import React from 'react';
+import { db } from "@/lib/db";
 
-const BoardIdPage: React.FC = () => {
+import { ListContainer } from "./_components/list-container";
+
+interface BoardIdPageProps {
+  params: {
+    boardId: string;
+  };
+};
+
+const BoardIdPage = async ({
+  params,
+}: BoardIdPageProps) => {
+  const { orgId } = auth();
+
+  if (!orgId) {
+    redirect("/select-org");
+  }
+  
+  const lists = await db.list.findMany({
+    where: {
+      boardId: params.boardId,
+      board: {
+        orgId,
+      },
+    },
+    include: {
+      cards: {
+        orderBy: {
+          order: "asc",
+        },
+      },
+    },
+    orderBy: {
+      order: "asc",
+    },
+  });
+
   return (
-    <div>
-     board
+    <div className="p-4 h-full overflow-x-auto">
+      <ListContainer
+        boardId={params.boardId}
+        data={lists}
+      />
     </div>
   );
 };
