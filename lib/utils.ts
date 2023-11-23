ccba188565c43baef74de214e48a1100aeb01225
Jari <jarirapani.turunen@gmail.com>
2023-11-23 13:23:17 +0200

    auditLog2

--- lib/utils.ts
+++ lib/utils.ts
@@ -4,7 +4,7 @@ import { twMerge } from "tailwind-merge"
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
-
+//lisätty 2022-11-23
 export function absoluteUrl(path: string) {
   return `${process.env.NEXT_PUBLIC_APP_URL}${path}`;
 };
